43ecae903a5872e0f2087ab795c76ddae3349dfd
Yaroslav <dombrovskiy91@gmail.com>
2021-01-09 11:55:04 +0200

    task page marckup + litle logic

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,14 +1,67 @@
 <template>
-  <div>
-    <div v-if="task">
+  <div class="row">
+    <div v-if="task" class="col s6 offset-s3">
       <h2>{{ task.title }}</h2>
+      <form @submit.prevent="submitHandler">
+        <div class="input-field">
+        </div>
+        <div class="chips" ref="chips"></div>
+
+        <div class="input-field">
+          <textarea
+            id="description"
+            class="materialize-textarea"
+            v-model="description"
+          >
+          </textarea>
+          <!-- <label for="description"></label> -->
+          <span class="character-counter">{{ description.length }}/2050</span>
+        </div>
+        <input type="text" ref="datepicker" />
+        <button class="btn" type="submit">Create Task</button>
+        <button class="btn blue btn-finish" type="button">Finish Task</button>
+
+      </form>
     </div>
     <p v-else>Task not found</p>
   </div>
 </template>
 <script>
 export default {
-  name:'task',
+  name: "task",
+  data() {
+    return {
+      description: '',
+      chips: null,
+      date: null
+    }
+  },
+  mounted() {
+    this.description = this.task.description
+    this.chips = M.Chips.init(this.$refs.chips, {
+      placeholder: "Task tags",
+      data: this.task.tags
+    }),
+    this.date = M.Datepicker.init(this.$refs.datepicker,{
+      format: 'dd.mm.yyyy',
+      defaultDate: new Date(),
+      setDefaultDate: true
+    });
+  },
+  methods: {
+    submitHandler() {
+      this.$store.dispatch('createTask', task)
+      this.$router.push('/list')
+    },
+  },
+  destroyed() {
+    if(this.date && this.date.destroy) {
+      this.date.destroy()
+    }
+    if(this.chips && this.chips.destroy) {
+      this.chips.destroy()
+  }
+  },
   computed: {
     task() {
       // console.log(typeof this.$route.params.id);
@@ -19,4 +72,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+  .btn-finish {
+    margin-left: 1rem;
+  }
 </style>
