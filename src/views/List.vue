653db90c313fba22ee3bb663fbd850abe161cdea
Yaroslav <dombrovskiy91@gmail.com>
2021-01-10 19:02:07 +0200

    markup for filter

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <h2>List</h2>
+    <div class="row">
+      <div class="input-field col s6">
+        <select ref="select">
+          <option value="" disabled selected>Choose your option</option>
+          <option value="active">Active</option>
+          <option value="outdated">Outdated</option>
+          <option value="complited">Complited</option>
+        </select>
+        <label>Status Filter</label>
+      </div>
+    </div>
+
     <hr />
     <table v-if="tasks.length">
       <thead>
@@ -18,15 +30,19 @@
         <tr v-for="(task, idx) of tasks" :key="task.id">
           <td>{{ idx + 1 }}</td>
           <td>{{ task.title }}</td>
-          <td>{{ new Date (task.date).toLocaleDateString() }}</td>
+          <td>{{ new Date(task.date).toLocaleDateString() }}</td>
           <td class="custom-td">
             <div class="text">{{ task.description }}</div>
           </td>
           <td>{{ task.status }}</td>
           <td>
-          <router-link tag="button" class="btn btn-small" :to="'/task/' + task.id">
-            Open
-          </router-link>
+            <router-link
+              tag="button"
+              class="btn btn-small"
+              :to="'/task/' + task.id"
+            >
+              Open
+            </router-link>
           </td>
         </tr>
       </tbody>
@@ -39,11 +55,14 @@
 
 <script>
 export default {
-  name: 'list',
+  name: "list",
   computed: {
     tasks() {
-      return this.$store.getters.tasks
+      return this.$store.getters.tasks;
     },
+  },
+  mounted() {
+    M.FormSelect.init(this.$refs.select);
   },
 };
 </script>
